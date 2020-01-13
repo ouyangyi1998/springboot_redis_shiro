5529909fbe78e729748d776d13e2aa531d231f2f
ouyangyi1998 <1347401764@qq.com>
2020-01-13 14:37:57 +0800

    1.0

--- src/main/java/com/jerry/springboot_redis_shiro/commom/config/ShiroConfig.java
+++ src/main/java/com/jerry/springboot_redis_shiro/commom/config/ShiroConfig.java
@@ -1,9 +1,19 @@
 package com.jerry.springboot_redis_shiro.commom.config;
 
+import com.baomidou.mybatisplus.extension.api.R;
+import com.jerry.springboot_redis_shiro.commom.shiro.ShiroRealm;
+import com.jerry.springboot_redis_shiro.commom.shiro.ShiroSessionIdGenerator;
+import com.jerry.springboot_redis_shiro.commom.shiro.ShiroSessionManager;
+import com.jerry.springboot_redis_shiro.commom.util.SHA256Util;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -45,10 +55,67 @@ public class ShiroConfig {
         return shiroFilterFactoryBean;
     }
     @Bean
-    public java.lang.SecurityManager securityManager()
+    public SecurityManager securityManager()
     {
         DefaultWebSecurityManager securityManager=new DefaultWebSecurityManager();
-
+        securityManager.setSessionManager(sessionManager());
+        securityManager.setCacheManager(redisCacheManager());
+        securityManager.setRealm(shiroRealm());
+        return securityManager;
     }
-
+    @Bean
+    public ShiroRealm shiroRealm()
+    {
+        ShiroRealm shiroRealm=new ShiroRealm();
+        shiroRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return shiroRealm;
+    }
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher()
+    {
+        HashedCredentialsMatcher hashedCredentialsMatcher=new HashedCredentialsMatcher();
+        hashedCredentialsMatcher.setHashAlgorithmName(SHA256Util.HASH_ALGORITHM_NAME);
+        hashedCredentialsMatcher.setHashIterations(SHA256Util.HASH_ITERATIONS);
+        return hashedCredentialsMatcher;
+    }
+    @Bean
+    public RedisManager redisManager()
+    {
+        RedisManager redisManager=new RedisManager();
+        redisManager.setHost(host);
+        redisManager.setPort(port);
+        redisManager.setTimeout(timeout);
+        return redisManager;
+    }
+    @Bean
+    public RedisCacheManager redisCacheManager()
+    {
+        RedisCacheManager redisCacheManager=new RedisCacheManager();
+        redisCacheManager.setRedisManager(redisManager());
+        redisCacheManager.setKeyPrefix(CACHE_KEY);
+        redisCacheManager.setPrincipalIdFieldName("userId");
+        return redisCacheManager;
+    }
+    @Bean
+    public ShiroSessionIdGenerator shiroSessionIdGenerator()
+    {
+        return new ShiroSessionIdGenerator();
+    }
+    @Bean
+    public RedisSessionDAO redisSessionDAO()
+    {
+        RedisSessionDAO redisSessionDAO=new RedisSessionDAO();
+        redisSessionDAO.setRedisManager(redisManager());
+        redisSessionDAO.setSessionIdGenerator(shiroSessionIdGenerator());
+        redisSessionDAO().setKeyPrefix(SESSION_KEY);
+        redisSessionDAO.setExpire(EXPIRE);
+        return redisSessionDAO;
+    }
+    @Bean
+    public SessionManager sessionManager()
+    {
+    ShiroSessionManager shiroSessionManager=new ShiroSessionManager();
+    shiroSessionManager.setSessionDAO(redisSessionDAO());
+    return shiroSessionManager;
+    }
 }
