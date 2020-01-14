8839d8de9ff45eed44affc98ae6e2f34b3c37d80
ouyangyi1998 <1347401764@qq.com>
2020-01-14 08:59:26 +0800

    2.0 debug

--- src/main/java/com/jerry/springboot_redis_shiro/commom/util/SHA256Util.java
+++ src/main/java/com/jerry/springboot_redis_shiro/commom/util/SHA256Util.java
@@ -4,8 +4,8 @@ import org.apache.shiro.crypto.hash.SimpleHash;
 
 public class SHA256Util {
     private SHA256Util(){}
-    public final static String HASH_ALGORITHM_NAME="SHA_256";
-    public final static int HASH_ITERATIONS=15;
+    public final static String HASH_ALGORITHM_NAME="SHA-256";
+    public final static int HASH_ITERATIONS=5;
     public static String sha256(String password,String salt)
     {
         return new SimpleHash(HASH_ALGORITHM_NAME,password,salt,HASH_ITERATIONS).toString();
